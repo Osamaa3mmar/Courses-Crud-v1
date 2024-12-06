449f0587467babe9163496f18701f94d0dc5ddfd
Your Name <you@example.com>
2024-12-06 08:43:24 +0200

    search+edit

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -5,9 +5,13 @@ const courseTagInput=document.querySelector(".course-Tag-Input");
 const courseDescriptionInput=document.querySelector(".course-Description-Input");
 const addBtn=document.querySelector(".add-btn");
 const cardsContainer=document.querySelector(".cards-row");
-const deleteAllBtn=document.querySelector(".delete-all-btn")
+const deleteAllBtn=document.querySelector(".delete-all-btn");
+const editFormBtn=document.querySelector(".edit-form-btn");
+const openAddDialog=document.querySelector(".open-add-dialog");
+const searchBar=document.querySelector(".search-bar-input");
 let enableImage=false;
 let courses=[];
+let currentCourse=-1;
 let localStorageInfoLoad=()=>{
     if(JSON.parse(localStorage.getItem("courses")) != null){
         courses=JSON.parse(localStorage.getItem("courses"));
@@ -15,13 +19,18 @@ let localStorageInfoLoad=()=>{
     
 }
 
-
+openAddDialog.onclick=()=>{
+  editFormBtn.disabled=true;
+  addBtn.disabled=false;
+  clearInputs();
+}
 
 addBtn.addEventListener("click",function(){
     //! here i should do the validation 
 
 
     //! here i should do the validation
+    
     let course={
         title:courseTitleInput.value,
         capacity:courseCapacityInput.value,
@@ -34,11 +43,7 @@ addBtn.addEventListener("click",function(){
     courses.push(course);
     localStorage.setItem("courses",JSON.stringify(courses));
     loadCourses();
-    courseTitleInput.value="";
-    courseCapacityInput.value="";
-    coursePriceInput.value="";
-    courseTagInput.value="";
-    courseDescriptionInput.value="";
+    clearInputs();
 
     Swal.fire({
         title: "Course Adedd",
@@ -49,14 +54,20 @@ addBtn.addEventListener("click",function(){
 
 
 });
+function clearInputs(){
+  courseTitleInput.value="";
+  courseCapacityInput.value="";
+  coursePriceInput.value="";
+  courseTagInput.value="";
+  courseDescriptionInput.value="";
+}
 
-
- function loadCourses(){
-    if(courses.length==0){
+ function loadCourses(items=courses){
+    if(items.length==0){
     cardsContainer.innerHTML=`No Courses Added Yet (You Can Add From The Plus Button)`;
     }
     else{
-    cardsContainer.innerHTML=courses.map((item,i)=>{
+    cardsContainer.innerHTML=items.map((item,i)=>{
     return `
     <div class="col-lg-4 col-md-6 col-sm-12 card-cont">
           <div class="card " >
@@ -69,8 +80,7 @@ addBtn.addEventListener("click",function(){
               <div class="course-price mb-2 fw-semibold ">${item.price} <span class="text-success">$</span></div>
               <div class="course-capacity mb-2 text-secondary">${item.capacity} <i class="fa-solid fa-user"></i></div>
             </div>
-              
-              <a href="#" class="btn btn-primary">Edit</a>
+              <button type="button" class="btn  btn-primary " data-bs-toggle="modal" onclick="editBtn(${i})" data-bs-target="#exampleModal">Edit</button>
               <a href="#" class="btn btn-danger"onclick="deleteBtn(${i})">Delete</a>
             </div>
           </div>
@@ -130,6 +140,57 @@ deleteAllBtn.onclick=async ()=>{
 }
 }
 
-localStorageInfoLoad();
-console.log(courses)
+editFormBtn.onclick=()=>{
+  let course={
+    title:courseTitleInput.value,
+    capacity:courseCapacityInput.value,
+    price:coursePriceInput.value,
+    tag:courseTagInput.value,
+    description:courseDescriptionInput.value
+}
+
+courses[currentCourse]=course;
+localStorage.setItem("courses",JSON.stringify(courses));
 loadCourses();
+Swal.fire({
+  title: "Course Edited Successfully",
+  text: "",
+  icon: "success"
+});
+}
+
+function editBtn(index){
+  currentCourse=index;
+  clearInputs();
+  editFormBtn.disabled=false;
+  addBtn.disabled=true;
+  courseTitleInput.value=courses[index].title;
+  courseCapacityInput.value=courses[index].capacity;
+  coursePriceInput.value=courses[index].price;
+  courseTagInput.value=courses[index].tag;
+  courseDescriptionInput.value=courses[index].description;
+}
+
+
+
+searchBar.addEventListener("keyup",function(e){
+  let sentanse=e.target.value;
+  console.log(e.target.value);
+  let searched=courses.filter((item)=>{
+    return item.title.includes(sentanse);
+  });
+  if(searched.length >0){
+    loadCourses(searched);
+  }
+  else{
+    loadCourses();
+  }
+})
+
+
+localStorageInfoLoad();
+loadCourses();
+editFormBtn.setAttribute("disabled","disabled");
+addBtn.setAttribute("disabled","disabled");
+
+
